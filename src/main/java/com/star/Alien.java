970e5ef1c6f5f03173e58ee350360e1803237418
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-12-12 11:55:20 +0600

    Initial setup - Spring

---
+++ src/main/java/com/star/Alien.java
@@ -0,0 +1,11 @@
+package com.star;
+
+public class Alien {
+    public void greeting() {
+        System.out.println("Hello World!");
+    }
+
+    public void code() {
+        System.out.println("Coding...");
+    }
+}
